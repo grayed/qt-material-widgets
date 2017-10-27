eca775d6072ec04c3b551e2e69e198c2325f5725
hunt978 <bootsing.hoo@gmail.com>
2017-10-27 22:51:24 +0800

    move auto complete upper for showing full dropdown list

--- demons/mainsheet.cpp
+++ demons/mainsheet.cpp
@@ -165,6 +165,21 @@ MainSheet::MainSheet(QWidget* parent) : QWidget(parent) {
   layout_a->addWidget(circular_prog);
   layout_a->addSpacing(80);
 
+  // auto complete
+  auto* autocomplete = new QtMaterialAutoComplete(sheet_a);
+  autocomplete->setDataSource({ "Alabama", "Alaska", "American Samoa", "Arizona",
+	  "Arkansas", "California", "Colorado", "Connecticut", "Delaware",
+	  "District of Columbia", "Florida", "Georgia", "Guam", "Hawaii", "Idaho",
+	  "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine",
+	  "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi",
+	  "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire",
+	  "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota",
+	  "Northern Marianas Islands", "Ohio", "Oklahoma", "Oregon", "Pennsylvania",
+	  "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota",
+	  "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Virgin Islands",
+	  "Washington", "West Virginia", "Wisconsin", "Wyoming" });
+  layout_a->addWidget(autocomplete);
+
   // QtMaterialCheckBox
   auto* check_box = new QtMaterialCheckBox(sheet_a);
   check_box->setText("hello meterial");
@@ -174,21 +189,6 @@ MainSheet::MainSheet(QWidget* parent) : QWidget(parent) {
   // QtMaterialFlatButton
   auto* flat_button = new QtMaterialFlatButton("Flat Button", sheet_a);
   layout_a->addWidget(flat_button);
-
-  // auto complete
-  auto* autocomplete = new QtMaterialAutoComplete(sheet_a);
-  autocomplete->setDataSource({"Alabama", "Alaska", "American Samoa", "Arizona",
-      "Arkansas", "California", "Colorado", "Connecticut", "Delaware",
-      "District of Columbia", "Florida", "Georgia", "Guam", "Hawaii", "Idaho",
-      "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine",
-      "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi",
-      "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire",
-      "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota",
-      "Northern Marianas Islands", "Ohio", "Oklahoma", "Oregon", "Pennsylvania",
-      "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota",
-      "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Virgin Islands",
-      "Washington", "West Virginia", "Wisconsin", "Wyoming"});
-  layout_a->addWidget(autocomplete);
 
   // scroll bar
   auto* scrollbar = new QtMaterialScrollBar();
